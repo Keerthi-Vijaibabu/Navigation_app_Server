15caac2ee71337960cd8a8319c9fec4175efaaab
Keerthi-Vijaibabu <keerthi.vijaibabu@gmail.com>
2026-01-24 08:51:05 +0530

    Server in working condition

---
+++ main.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI
+import joblib
+from pydantic import BaseModel
+
+app = FastAPI(title="Indoor Localisation API")
+
+model = joblib.load("./models/linear_model.pkl")
+
+
+# ----------- Nested models -----------
+class MagData(BaseModel):
+    x: float
+    y: float
+    z: float
+    ts: int | None = None
+
+
+class GpsData(BaseModel):
+    lat: float
+    lon: float
+    ts: int | None = None
+
+
+class PredictRequest(BaseModel):
+    room: str | None = None
+    mag: MagData
+    gps: GpsData
+
+
+@app.post("/predict")
+def predict(req: PredictRequest):
+    print("REQUEST RECEIVED:", req)
+
+    X = [[
+        req.gps.lat,
+        req.gps.lon,
+        req.mag.x,
+        req.mag.y,
+        req.mag.z,
+        (req.mag.x**2 + req.mag.y**2 + req.mag.z**2) ** 0.5  # magnitude
+    ]]
+
+    y = model.predict(X)
+
+    return {
+        "ok": True,
+        "x": float(y[0][0]),
+        "y": float(y[0][1]),
+    }
+
+
+@app.post("/hello_world")
+def hello_world():
+    return {"hello": "world"}
+
+
+
+#to run
+#python -m uvicorn main:app --host 0.0.0.0 --port 8000 --reload
